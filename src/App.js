21fb844ae75c043c2ac847464629457a64f842f0
jimmymendy <jimmymendy@Pros-MacBook-Pro.local>
2022-03-15 14:08:20 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./CSS/App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./components/MovieCard";
+
+const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const { Search } = await response.json();
+    // console.log(Search);
+    setMovies(Search);
+  };
+
+  useEffect(() => {
+    searchMovies('French');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Movie Park</h1>
+
+      <div className='search'>
+        <input
+          type='text'
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt='Search Icon'
+          onClick={() => {
+            searchMovies(searchTerm)
+          }}
+        />
+      </div>
+          
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
